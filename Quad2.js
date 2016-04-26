be7852e37e7bf87f6f58565fcb9acc0258856e20
Tamás <icyplusplus@gmail.com>
2016-04-26 22:59:56 +0200

    Test.

--- Quad2.js
+++ Quad2.js
@@ -45,9 +45,9 @@ Quad.prototype.draw = function(gl, camera, width, height, framesSinceLastAction)
           false, 0,
           0);
 
-    var viewDirMatrixData = new Float32Array(16);
-    camera.viewDirMatrix.copyIntoArray(viewDirMatrixData, 0);
-    gl.uniformMatrix4fv(this.viewDirMatrixLocation, false, viewDirMatrixData);
+    // var viewDirMatrixData = new Float32Array(16);
+    // camera.viewDirMatrix.copyIntoArray(viewDirMatrixData, 0);
+    // gl.uniformMatrix4fv(this.viewDirMatrixLocation, false, viewDirMatrixData);
 
     // gl.uniform3f(this.eyeLocation, camera.position.x, camera.position.y, camera.position.z);
     // gl.uniformMatrix4fv(this.quadricsLocation, false, this.quadricData);
